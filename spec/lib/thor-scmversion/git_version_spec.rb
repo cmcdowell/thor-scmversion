95cd8bb0c58fdd5cfff23d802074d33a338dd528
Christopher McDowell <t-cmcdowell@ean.com>
2016-07-18 14:19:58 +0100

    Update test to use new syntax.

--- spec/lib/thor-scmversion/git_version_spec.rb
+++ spec/lib/thor-scmversion/git_version_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 module ThorSCMVersion
   describe GitVersion do
-    it "should detect if a commit is contained on a given branch" do
-      ShellUtils.stub(:sh).and_return(<<OUT)
+    it "should detect if a tag is contained on a given branch" do
+      allow(ShellUtils).to receive(:sh).and_return(<<OUT)
 * constrain_bump_to_branch
   master
 OUT
-      expect(GitVersion.contained_in_current_branch?('0.0.1')).to be_true
+      expect(GitVersion.contained_in_current_branch?('0.0.1')).not_to be_nil
     end
   end
 end
